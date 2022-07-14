fef02699924c23c857d5643750fe4e158a8e3649
claireeeeey <90796587+claireeeeey@users.noreply.github.com>
2022-07-14 10:32:16 -0500

    Update index.js

--- basic/scripts/index.js
+++ basic/scripts/index.js
@@ -1,3 +1,4 @@
+//Create question and answer object
 class cards {
   constructor(question, answer) {
     this.question = question;
@@ -9,11 +10,13 @@ let front = document.getElementById("front");
 let back = document.getElementById("back");
 let quiz = [];
 
+//Create four question sets and push them onto the quiz array
 quiz.push(new cards('What is 3+5?', 'more than six?'));
 quiz.push(new cards('What color is the sky?', 'blue'));
 quiz.push(new cards('Is ice cold?', 'yes'));
 quiz.push(new cards('what date is today?', 'idk'));
 
+//Use while loop to iterate over the array, print out the question and add a listener
 let i = 0;
 while (i < quiz.length)  {
   front.innerHTML = quiz[i].question;
@@ -24,6 +27,7 @@ while (i < quiz.length)  {
   ++i
 }
 
+//Function for the previous and next button to display the question one at a time
 let cardindex = 0;
 function next() {
   cardindex = (cardindex + 1) % quiz.length;
